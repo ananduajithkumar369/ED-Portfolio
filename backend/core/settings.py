86ee89168d9c2f1184744dae8bebae57da2f6635
anandu <ananduajithkumar369@gmail.com>
2026-05-25 10:56:31 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p=a026jy3$9hacu@__zg-&%pb3b^%tmuquggmmwirla))ers!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ed-portfolio-rdd3.onrender.com"]
 
 
 # Application definition
